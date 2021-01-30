e2fc39d6c26c9534ca693cedd90ae356a9a0a285
Mirella <ruiz.mirella27@gmail.com>
2021-01-30 10:59:17 -0430

    separe el js

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+window.sr = ScrollReveal({ reset: true });
+      sr.reveal(".navbar", {
+        duration: 2000,
+        origin: "bottom",
+      });
+      sr.reveal(".header-content-left", {
+        duration: 2000,
+        origin: "top",
+        distance: "300px",
+      });
+      sr.reveal(".header-content-right", {
+        duration: 2000,
+        origin: "right",
+        distance: "300px",
+      });
+      sr.reveal(".header-btn", {
+        duration: 2000,
+        origin: "bottom",
+        delay: 2000,
+      });
+      sr.reveal("#testimonial", {
+        duration: 2000,
+        origin: "left",
+        distance: "300px",
+        viewFactor: 0.2,
+      });
+
+      //scroll script
+
+      document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+          });
+        });
+      });
